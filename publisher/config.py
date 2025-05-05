37c7a201b02f75b8358b9f20882326260095e171
RapTho <raphael.tholl@protonmail.com>
2025-05-05 11:45:36 +0200

    handle no .env file found

--- publisher/config.py
+++ publisher/config.py
@@ -1,15 +1,14 @@
 import os
+import json
 from dotenv import load_dotenv
 
-load_dotenv()
+try:
+    load_dotenv()
+except FileNotFoundError:
+    print("No .env file found")
 
 class Config:
-    BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "localhost")
-    USERNAME = os.getenv("USERNAME", "student1")
-    PASSWORD = os.getenv("PASSWORD", "password1")
-    TOPIC = os.getenv("TOPIC", "student1/topic")
-    PORT = int(os.getenv("PORT", 1883))
-    SCHEMA = {
+    DEFAULT_SCHEMA = {
         "type": "object",
         "properties": {
             "id": {"type": "integer"},
@@ -17,3 +16,9 @@ class Config:
         },
         "required": ["id", "message"]
     }
+    BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "localhost")
+    USERNAME = os.getenv("USERNAME", "student1")
+    PASSWORD = os.getenv("PASSWORD", "password1")
+    TOPIC = os.getenv("TOPIC", "student1/topic")
+    PORT = int(os.getenv("PORT", 1883))
+    SCHEMA = json.loads(os.getenv("SCHEMA", json.dumps(DEFAULT_SCHEMA)))
